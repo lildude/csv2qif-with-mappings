51f64bcc5ecffa25dfbc5260d4a46aa0b01eb936
Colin Seymour <lildood@gmail.com>
2016-01-31 15:14:44 +0000

    Add prelim support for !Account for when the PR lands

--- csv2qif.rb
+++ csv2qif.rb
@@ -55,24 +55,32 @@ bank_file = File.read(file_path).force_encoding('utf-8')
 bank_file = CSV.parse(bank_file, col_sep: acc_cfg[:col_sep])
 
 
-
 Qif::Writer.open(output_file, type = acc_cfg[:type], format = acc_cfg[:date_fmt]) do |qif|
+  # Uncomment this when https://github.com/jemmyw/Qif/pull/13 is merged.
+  #qif << Qif::Account.new(
+  #  :name         => acc_cfg[:name],
+  #  :type         => acc_cfg[:type],
+  #  :description  => ( acc_cfg[:description] unless acc_cfg[:description].nil? ),
+  #  :limit        => ( acc_cfg[:limit] unless acc_cfg[:limit].nil? ),
+  #  :balance_date => ( acc_cfg[:balance_date] unless acc_cfg[:balance_date].nil? ),
+  #  :balance      => ( acc_cfg[:balance] unless acc_cfg[:balance].nil? ),
+  #)
   bank_file.each do |row|
-    next if ( row.empty? || row[0] == "Date" )
+    next if ( row.empty? || row[0] == "Date" )  # Assumes the CSV file header line starts with "Date"
     row.each { |value| value.to_s.gsub!(/^\s+|\s+$/,'') }
     date = row[0].split("/").reverse.join("/")
     qif << Qif::Transaction.new(
-        :date               => row[acc_cfg[:csv_field_map][:date]],
-        :amount             => calculate_amount(row, acc_cfg),
-        :payee              => row[acc_cfg[:csv_field_map][:payee]],
-        :number             => ( row[acc_cfg[:csv_field_map][:number]].gsub!(/.*/, stringify_keys(acc_cfg[:number_map])) unless acc_cfg[:csv_field_map][:number].nil? ),
-        :memo               => ( row[acc_cfg[:csv_field_map][:memo]] unless acc_cfg[:csv_field_map][:memo].nil? ),
-        :status             => ( row[acc_cfg[:csv_field_map][:status]] unless acc_cfg[:csv_field_map][:status].nil? ),
-        :adress             => ( row[acc_cfg[:csv_field_map][:adress]] unless acc_cfg[:csv_field_map][:adress].nil? ),
-        :category           => ( row[acc_cfg[:csv_field_map][:category]] unless acc_cfg[:csv_field_map][:category].nil? ),
-        :split_category     => ( row[acc_cfg[:csv_field_map][:split_category]] unless acc_cfg[:csv_field_map][:split_category].nil? ),
-        :split_memo         => ( row[acc_cfg[:csv_field_map][:split_memo]] unless acc_cfg[:csv_field_map][:split_memo].nil? ),
-        :split_amount       => ( row[acc_cfg[:csv_field_map][:split_amount]] unless acc_cfg[:csv_field_map][:split_amount].nil? ),
-      )
+      :date               => row[acc_cfg[:csv_field_map][:date]],
+      :amount             => calculate_amount(row, acc_cfg),
+      :payee              => row[acc_cfg[:csv_field_map][:payee]],
+      :number             => ( row[acc_cfg[:csv_field_map][:number]].gsub!(/.*/, stringify_keys(acc_cfg[:number_map])) unless acc_cfg[:csv_field_map][:number].nil? ),
+      :memo               => ( row[acc_cfg[:csv_field_map][:memo]] unless acc_cfg[:csv_field_map][:memo].nil? ),
+      :status             => ( row[acc_cfg[:csv_field_map][:status]] unless acc_cfg[:csv_field_map][:status].nil? ),
+      :adress             => ( row[acc_cfg[:csv_field_map][:adress]] unless acc_cfg[:csv_field_map][:adress].nil? ),
+      :category           => ( row[acc_cfg[:csv_field_map][:category]] unless acc_cfg[:csv_field_map][:category].nil? ),
+      :split_category     => ( row[acc_cfg[:csv_field_map][:split_category]] unless acc_cfg[:csv_field_map][:split_category].nil? ),
+      :split_memo         => ( row[acc_cfg[:csv_field_map][:split_memo]] unless acc_cfg[:csv_field_map][:split_memo].nil? ),
+      :split_amount       => ( row[acc_cfg[:csv_field_map][:split_amount]] unless acc_cfg[:csv_field_map][:split_amount].nil? ),
+    )
   end
 end
